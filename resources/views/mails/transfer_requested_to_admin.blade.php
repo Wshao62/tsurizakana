903c54c0046b3f8f045f1f2f639947c820537c7d
Seiko Tsubono <tsubono@wss.systems>
2019-12-29 21:24:37 +0900

    メールに振込申請番号追加

--- resources/views/mails/transfer_requested_to_admin.blade.php
+++ resources/views/mails/transfer_requested_to_admin.blade.php
@@ -4,6 +4,7 @@
 振込予定日：{{ $transfer_at_ja }}<br>
 振込申請日：{{ $requested_at_ja }}<br>
 ユーザーID：{{ $transfer_request->user_id }}<br>
+振込申請番号：{{ $transfer_request->application_number }}<br>
 ユーザー名：{{ $transfer_request->user->name }}<br>
 実際の振込額：{{ number_format($transfer_request->transfer_price) }}円
 @endsection
--- resources/views/mails/transfer_requested_to_user.blade.php
+++ resources/views/mails/transfer_requested_to_user.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 以下の内容で振込申請を受け付けました。<br>
+振込申請番号：{{ $transfer_request->application_number }}<br>
 振込申請額：{{ number_format($transfer_request->price) }}円<br>
 振込手数料：{{ number_format($transfer_request->fee) }}円<br>
 実際の振込額：{{ number_format($transfer_request->transfer_price) }}円
